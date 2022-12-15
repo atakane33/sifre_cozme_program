f9441b4ff2a40bdf002aabb784b7d96c7a82090c
İbrahim Atakan Elkuş <117391447+atakane33@users.noreply.github.com>
2022-12-15 15:36:18 +0300

    Add files via upload

---
+++ Şifre Çözme Programı/Şifre Çözme Programı/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Şifre_Çözme_Programı
+{
+    class Program
+    {
+        public string sifrelepar;
+        public string sifrecozpar;
+        public string sifrele;
+
+        public string Kendi(string yazıpar)
+        {
+            sifrele = yazıpar;
+
+            return sifrele;
+        }
+        public string Sifrele(string yazıpar)
+        {
+
+            string sifreSifrele = "";
+
+            foreach (char x in yazıpar)                                       
+            {
+                
+                int sifreleme = (int)x + 1;
+
+                sifreSifrele += (char)sifreleme;               
+            
+            }
+
+            sifrelepar = sifreSifrele;
+
+            return sifreSifrele;
+           
+        }
+
+
+        public string Sifre_Coz(string yazıpar)
+        {
+
+            yazıpar = sifrelepar;
+
+            string sifreCoz = "";
+
+            foreach (char a in yazıpar)
+            {
+                
+                int sifreleme = (int)a - 1;
+
+                sifreCoz += (char)sifreleme;
+
+            }
+
+            sifrecozpar = sifreCoz;
+
+            return sifreCoz;
+        }
+
+        public void Yaz()
+        {
+            Console.WriteLine("Alınan Metin : {0} ", sifrele);
+
+            Console.WriteLine("Şifrelenmiş Metin : {0} ", sifrelepar);
+
+            Console.WriteLine("Şifresi Çözüldükten Sonraki Metin : {0} ", sifrecozpar);
+        }
+
+        
+        static void Main(string[] args)
+        {
+
+            Program nesne = new Program();
+
+            Console.Write("Şifrelenmesi için bir yazı yazınız : ");
+
+            string yazı = Console.ReadLine();
+
+            if (yazı.Length == 0)
+            {
+                Console.WriteLine("Şifrelenecek bir metin yok ya da hatalı giriş ! ");
+            }
+
+            nesne.Kendi(yazı);
+            
+            nesne.Sifrele(yazı);
+
+            nesne.Sifre_Coz(yazı);
+
+            nesne.Yaz();
+
+
+        }
+    }
+}
---
+++ Şifre Çözme Programı/Şifre Çözme Programı/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Şifre Çözme Programı")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Şifre Çözme Programı")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("fbe7acf4-9fe9-473d-9436-8a88705be0bf")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
